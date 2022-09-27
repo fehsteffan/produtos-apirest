6a6792db8a7d14356bf85655aea30f1a7bca65fa
Felipe Steffan <felip.developer@hotmail.com>
2022-09-26 23:11:03 -0300

    iniciando api-rest

---
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -0,0 +1,60 @@
+package com.produtos.apirest.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto implements Serializable {	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private BigDecimal quantidade;
+	private BigDecimal valor;
+	
+	public Produto () {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return nome;
+	}
+
+	public void setName(String name) {
+		this.nome = name;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('samsumg sB', '1.00', '2599.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('notebook lenovo', '2.00', '3500.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('mac book', '1.00', '8000.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('iphone 8', '2.00', '4500.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('moto g4', '1.00', '1500.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('smart tv lg50', '1.00', '3550.00');
+INSERT INTO tb_produto (nome, quantidade, valor) VALUES ('smart tv lg65', '1.00', '4990');
+
+
