ee27ca5896d9d78c6687d19d3912dff4e90995ea
Felipe Steffan <felip.developer@hotmail.com>
2022-09-27 14:36:26 -0300

    Inserindo Swagger-ui

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.services.ProdutoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/produtos")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 	
 	@Autowired
@@ -27,12 +33,14 @@ public class ProdutoResource {
 	
 	
 	@GetMapping
+	@ApiOperation(value="Retorna uma lista de produtos")
 	public ResponseEntity<List<Produto>> findAll() {
 		List<Produto> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value="/{id}")
+	@ApiOperation(value="Retorna produto por id")
 	public ResponseEntity<Produto> findById(@PathVariable Long id){
 		Produto obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);				
@@ -40,6 +48,7 @@ public class ProdutoResource {
 	}
 	
 	@PostMapping
+	@ApiOperation(value="Insere um novo produto")
 	public ResponseEntity<Produto> insert(@RequestBody Produto obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -48,12 +57,14 @@ public class ProdutoResource {
 	
 	
 	@DeleteMapping(value="/{id}")
+	@ApiOperation(value="Deleta um produto")
 	public ResponseEntity<Void> delete (@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping(value="/{id}")
+	@ApiOperation(value="Atualiza um produto")
 	public ResponseEntity<Produto> update (@PathVariable Long id, @RequestBody Produto produto) {
 		produto = service.update(produto);
 		return ResponseEntity.ok().body(produto);		
